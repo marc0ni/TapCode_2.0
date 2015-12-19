6c4216ad2323c528eb90c7329b5e48f4d6fdf45a
Mark Lindamood <mark.lindamood@yahoo.com>
2015-12-19 13:58:38 -0800

    Initial commit

---
+++ SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TapCode_2.0
+//
+//  Created by Mark Lindamood on 12/17/15.
+//  Copyright © 2015 Mark Lindamood. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
