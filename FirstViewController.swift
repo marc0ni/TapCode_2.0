3014ab82bd3ad4456357b5854c45de6b183b18a0
Mark Lindamood <mark.lindamood@yahoo.com>
2015-12-20 11:52:08 -0800

    Adding Contacts to First View Controller

--- FirstViewController.swift
+++ FirstViewController.swift
@@ -7,19 +7,68 @@
 //
 
 import UIKit
+import Contacts
+import ContactsUI
 
-class FirstViewController: UIViewController {
-
+class ContactsViewController: UIViewController, CNContactPickerDelegate, CNContactViewControllerDelegate {
+    
+    var contactStore = CNContactStore()
+    var updateContact = CNContact()
+    
+    // MARK: - View LifeCycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        askForContactAccess()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    /*override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "OpenUpdateContactView" {
+            let addUpdateContactViewController = segue.destinationViewController as! AddUpdateContactViewController
+            addUpdateContactViewController.updateContact = updateContact
+            addUpdateContactViewController.isUpdate = true
+        }
+    }*/
+    
+    // MARK: - Action Methods
+    @IBAction func btnViewAllContactsClicked(sender: AnyObject) {
+        let contactPickerViewController = CNContactPickerViewController()
+        presentViewController(contactPickerViewController, animated: true, completion: nil)
     }
-
-
+    
+    // MARK: - Contact Access Permission Method
+    func askForContactAccess() {
+        let authorizationStatus = CNContactStore.authorizationStatusForEntityType(CNEntityType.Contacts)
+        
+        switch authorizationStatus {
+        case .Denied, .NotDetermined:
+            self.contactStore.requestAccessForEntityType(CNEntityType.Contacts, completionHandler: { (access, accessError) -> Void in
+                if !access {
+                    if authorizationStatus == CNAuthorizationStatus.Denied {
+                        dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                            let message = "\(accessError!.localizedDescription)\n\nPlease allow the app to access your contacts through the Settings."
+                            let alertController = UIAlertController(title: "Contacts", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+                            
+                            let dismissAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default) { (action) -> Void in
+                            }
+                            
+                            alertController.addAction(dismissAction)
+                            
+                            self.presentViewController(alertController, animated: true, completion: nil)
+                        })
+                    }
+                }
+            })
+            break
+        default:
+            break
+        }
+    }
+    
+    // MARK: - Contact Select Method
+    func contactPicker(picker: CNContactPickerViewController, didSelectContact contact: CNContact) {
+        updateContact = contact
+        self.dismissViewControllerAnimated(false, completion: nil)
+        performSegueWithIdentifier("OpenUpdateContactView", sender: nil)
+    }
 }
-
---
+++ FourthViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FourthViewController.swift
+//  TapCode_2.0
+//
+//  Created by Mark Lindamood on 12/20/15.
+//  Copyright © 2015 Mark Lindamood. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
